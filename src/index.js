292c09b662538196af30be038b83f748d941aff8
AyelenMillan <ayelen.millan@gmail.com>
2022-06-17 00:39:45 -0300

    Mipagina

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import {dirname, join} from 'path'
+import { fileURLToPath } from 'url'
+
+import indexRoutes from './routes/index.js' 
+
+// Initialize express
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+//settings
+app.set("port", process.env.PORT || 3000);
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+//routes
+app.use(indexRoutes)
+
+//static files
+app.use(express.static(join(__dirname, 'public')))
+
+
+//listening the server
+app.listen(3000)
+console.log('server is listening on port', 3000)
